e0d47b875c9d68ab0e4900b4a9cdc0aff176a047
saheb mohammadi <saheb.put92@gmail.com>
2022-06-02 17:18:54 +0430

    client-side Guard

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,9 +1,25 @@
 import Layout from "@/containers/Layout";
+import { signIn, useSession } from "next-auth/react";
 
 const Profile = () => {
+  const { data: session, status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      signIn();
+    },
+  });
+
+  if (status === "loading") {
+    return (
+      <Layout>
+        <div>Loading...</div>
+      </Layout>
+    );
+  }
+
   return (
     <Layout>
-      <h1>Profile page</h1>
+      <h1> {session.user.name}, wellcome to Profile page</h1>
     </Layout>
   );
 };
