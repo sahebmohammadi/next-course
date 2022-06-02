c8a6afe51b386d565688b2a63ef6eb5f74097b4e
saheb mohammadi <saheb.put92@gmail.com>
2022-06-02 15:36:08 +0430

    client-side Auth

--- src/containers/Layout/Header.js
+++ src/containers/Layout/Header.js
@@ -1,14 +1,20 @@
 import Link from "next/link";
-import { signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+  const { data: session, status } = useSession();
+  console.log({ session, status });
   return (
     <div className="bg-white  w-full shadow-lg">
       <nav className="flex justify-between py-4 mb-6 max-w-screen-xl mx-auto">
         <h1 className="font-bold">
           <a href="#">TodoList App</a>
         </h1>
-        <ul className="flex items-center gap-x-6">
+        <ul
+          className={`flex items-center gap-x-6 ${
+            status === "loading" && !session ? "opacity-0" : "opacity-100"
+          }`}
+        >
           <li>
             <Link href="/">
               <a>Home</a>
@@ -24,12 +30,16 @@ const Header = () => {
               <a>Profile</a>
             </Link>
           </li>
-          <li>
-            <button onClick={() => signIn("github")}>Sign in</button>
-          </li>
-          <li>
-            <button onClick={() => signOut()}>Sign Out </button>
-          </li>
+          {!session && status !== "loading" && (
+            <li>
+              <button onClick={() => signIn("github")}>Sign in</button>
+            </li>
+          )}
+          {session && (
+            <li>
+              <button onClick={() => signOut()}>Sign Out </button>
+            </li>
+          )}
         </ul>
       </nav>
     </div>
